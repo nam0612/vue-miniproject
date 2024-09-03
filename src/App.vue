f857b5a5b9cc670a3b36ae4db9cf6cad76658d3a
nam0612 <nguyenhainammoonligh@gmail.com>
2024-09-03 20:21:40 +0700

    upcode

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <template>
   <v-app class="">
     <!-- Sizes your content based upon application components -->
-    <v-main :class="{ 'pa-0': $route.path == '/' }">
+    <v-main>
       <!-- Provides the application the proper gutter -->
-      <v-container
-        fluid
-        class="pa-0"
-        :class="{
-          'fill-height': $route.path == '/prijava' || $route.name == 'NotFound',
-        }"
-      >
+      <v-container fluid class="pa-0">
         <!-- Ovdje se injektiraju pogledi -->
         <router-view></router-view>
       </v-container>
